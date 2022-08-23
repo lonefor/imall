681cafbe0acef92ee913c6f20fa1c8bcecdb6d8c
lonefor <2494557633@qq.com>
2022-08-23 09:09:34 +0800

    1t

--- imall-portal/src/main/java/com/situ/mall/service/impl/ProductServiceImpl.java
+++ imall-portal/src/main/java/com/situ/mall/service/impl/ProductServiceImpl.java
@@ -4,11 +4,13 @@ import com.situ.mall.mapper.ProductMapper;
 import com.situ.mall.pojo.Product;
 import com.situ.mall.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-
+@Service
 public class ProductServiceImpl implements IProductService {
     @Autowired
     private ProductMapper productMapper;
+
     @Override
     public Product selectById(Integer id) {
         return productMapper.selectByPrimaryKey(id);
